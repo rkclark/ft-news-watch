dedd842119de2b96b2d1a5de0898e7cfb3145b89
Rick Clark <rkingsleyclark@gmail.com>
2017-03-28 15:30:35 +0100

    Add test file for headlines API

---
+++ test/server/routes/headlinesAPITest.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../../app.js';
+
+chai.use(chaiHttp);
+
+describe('headlinesAPI', () => {
+
+  describe('/GET headlinesAPI', () => {
+      it('it should a JSON object containing query object and results array', (done) => {
+        chai.request(server)
+            .get('/headlines')
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.query).to.be.a('object');
+                expect(res.body.results).to.be.a('array');
+              done();
+            });
+      });
+  });
+
+});
