5a9fe8ea9e3410695022d4b9e190bb2b10d750fb
Rick Clark <rkingsleyclark@gmail.com>
2017-03-29 17:52:44 +0100

    Display FT headlines in o-card components on homepage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 const router = express.Router();
+import fetch from 'node-fetch';
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  fetch('http://localhost:3000/headlines')
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      const headlines = json.results[0].results;
+      res.render('index', { headlines: headlines } );
+    });
 });
 
 module.exports = router;
--- test/client/indexTest.js
+++ test/client/indexTest.js
@@ -20,4 +20,38 @@ describe('FT News Watch homepage', function() {
         expect(headerLink.getAttribute('href')).to.equal('https://www.ft.com/?edition=uk');
     });
 
+    it('Displays at least 20 headlines in card components', function () {
+        browser.url('http://localhost:3000');
+        const headlines = browser.elements('.o-card');
+        expect(headlines.value.length >= 20).to.be.true;
+    });
+
+    describe('Headline card content with class o-card__content', function() {
+
+      it('First child is a heading with link to the article on the FT', function () {
+          browser.url('http://localhost:3000');
+          const card = browser.element('.headlines-list .o-card:nth-child(1) .o-card__content h2:first-child').getHTML();
+          expect(card).to.contain('o-card__heading');
+          expect(card).to.contain('href="http://www.ft.com');
+      });
+
+      it('Second child is a paragraph with class o-card__standfirst', function () {
+          browser.url('http://localhost:3000');
+          const para = browser.element('.headlines-list .o-card:nth-child(1) .o-card__content p:nth-child(2)');
+          expect(para.getAttribute('class')).to.contain('o-card__standfirst');
+      });
+
+      it('Third child is a paragraph with class o-card__tag', function () {
+          browser.url('http://localhost:3000');
+          const para = browser.element('.headlines-list .o-card:nth-child(1) .o-card__content p:nth-child(3)');
+          expect(para.getAttribute('class')).to.contain('o-card__tag');
+      });
+
+      it('Fourth child is a time with class o-card__timestamp', function () {
+          browser.url('http://localhost:3000');
+          const para = browser.element('.headlines-list .o-card:nth-child(1) .o-card__content time:nth-child(4)');
+          expect(para.getAttribute('class')).to.contain('o-card__timestamp');
+      });
+    });
+
 });
