414e2e62d2e8e2a864a54575b05e446c07199eac
Rick Clark <rkingsleyclark@gmail.com>
2017-03-30 10:40:28 +0100

    Add responsive grid to headline o-cards

--- test/client/indexTest.js
+++ test/client/indexTest.js
@@ -111,6 +111,20 @@ describe('FT News Watch homepage', function() {
 
       });
 
+      describe('Responsive grid', function() {
+
+        it('Wraps headlines list in grid container', function() {
+          browser.url('http://localhost:3000');
+          expect(browser.getAttribute('.headlines-list', 'class')).to.contain('o-grid-container');
+        });
+
+        it('Applies responsive column widths to each headline o-card', function() {
+          browser.url('http://localhost:3000');
+          expect(browser.getAttribute('.headlines-list .o-card:nth-child(1)', 'data-o-grid-colspan')).to.equal('12 S10 Soffset1 M8 Moffset2 L6 Loffset0');
+        });
+
+      });
+
     });
 
 });
