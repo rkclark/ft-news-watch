ffe78edf02b8cd4063417ec17c4b1c33420fec75
Rick Clark <rkingsleyclark@gmail.com>
2017-03-31 15:06:19 +0100

    Add static file caching when in production

--- src/app.js
+++ src/app.js
@@ -38,7 +38,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); // eslint-disable-line
+
+if (process.env.NODE_ENV === 'production') { // eslint-disable-line
+  app.use(express.static(path.join(__dirname, 'public'))); // eslint-disable-line
+} else {
+  app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneDay })); // eslint-disable-line
+}
+
 
 app.use(headlinesAPI);
 app.use(index);
